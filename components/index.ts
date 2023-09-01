73c8564f7bf7c9691405e982701ffedcfa609f07
thekha3321 <thekha234@gmail.com>
2023-09-01 16:04:23 +0700

    get API from RAPID car

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,20 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManuFacturer from "./SearchManuFacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManuFacturer,
+    CarCard,
+    CarDetails,
+};
