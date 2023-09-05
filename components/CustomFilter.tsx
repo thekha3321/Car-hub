f47fe1521419266e96d9d2aa6c5c888105f1f99d
thekha3321 <thekha234@gmail.com>
2023-09-05 16:40:56 +0700

    update show more & filter cars

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -6,15 +6,10 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const router = useRouter();
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
 
-  const handleUpdateParams = (e: { title: string; value: string }) => {
-    const newPathName = updateSearchParams(title, e.value.toLowerCase());
-
-    router.push(newPathName);
-  };
+ 
 
   return (
      <div className='w-fit'>
@@ -22,7 +17,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
         value={selected}
         onChange={(e) => {
           setSelected(e); // Update the selected option in state
-          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+          setFilter(e.value); // Update the URL search parameters and navigate to the new URL
         }}
       >
         <div className="relative w-fit z-10">
